36a5db0037640ae7cb104924eaf5b05b44310bae
manojachanta72 <manojachanta7172@gmail.com>
2025-01-08 14:46:07 +0530

    configured auto-formmater

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         
 
-<nav className="bg-white border-gray-200 dark:bg-gray-900">
+<><nav className="bg-[#F9FAFB] border-gray-200 dark:bg-gray-900">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
@@ -56,7 +56,7 @@ const Navbar = () => {
     </div>
   </div>
 </nav>
-
+</>
     )
 }
 export default Navbar
