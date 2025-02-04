d504e6a419b88f72a809ca3f16b19605fd22530f
Gowthamssr <gowthamssr0@gmail.com>
2025-02-04 14:31:50 +0530

    superset-charts

--- src/App.jsx
+++ src/App.jsx
@@ -26,9 +26,38 @@ const App = () => {
           isSidebarOpen={isSidebarOpen}
         />
       </div>
-     <div className={`pt-16 flex-grow transition-all duration-300 ${isSidebarOpen ? 'ml-60' : 'ml-0'}`}> {/* Adjust padding based on your navbar height */}
-          <Body />
+      <div className={`pt-16 flex-grow transition-all duration-300 ${isSidebarOpen ? 'ml-60' : 'ml-0'}`}> {/* Adjust padding based on your navbar height */}
+        <Body />
+        <div className='flex-col  mt-[10%] text-center'>
+          <h1 className='font-500 bold'>Superset charts</h1>
+          <div className='flex m-[10%]'>
+            <div className='mr-[20px]'>
+
+              <iframe
+                className='mt-20px'
+                width="600"
+                height="400"
+                seamless
+                frameBorder="0"
+                scrolling="no"
+                src="http://localhost:8088/superset/explore/p/B6KW02Ql8EJ/?standalone=1&height=400"
+              >
+              </iframe>
+            </div>
+
+
+            <iframe
+              width="600"
+              height="400"
+              seamless
+              frameBorder="0"
+              scrolling="no"
+              src="http://localhost:8088/superset/explore/p/JnjXlY85bwW/?standalone=1&height=400"
+            >
+            </iframe>
+          </div>
         </div>
+      </div>
     </div>
   );
 };
